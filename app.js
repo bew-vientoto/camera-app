f1221c0a8c9c94af214ab6b44bea1e3b06dcfc7e
bew-vientoto <bewvientoto@gmail.com>
2020-07-07 12:00:45 +0700

    test

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-function readImage(file) {
-  // Check if the file is an image.
-  if (file.type && file.type.indexOf('image') === -1) {
-    console.log('File is not an image.', file.type, file);
-    return;
-  }
+let fileInput = document.getElementById('fileinput');
 
-  const reader = new FileReader();
-  reader.addEventListener('load', (event) => {
-    img.src = event.target.result;
-  });
-  reader.readAsDataURL(file);
-}
+fileInput.addEventListener('change', 
+                           function(ev) {
+  console.log("hi");
+    console.log(ev.target.files);
+  if(ev.target.files) {
+    let file = ev.target.files[0];
+    var reader  = new FileReader();
+
+    reader.onloadend = function (e) {
+      var image = new Image();
+      image.src = e.target.result;
+      image.onload = function(ev) {
+        console.log("loading");
+        var canvas = document.getElementById('canvas');
+        canvas.width = image.width;
+        canvas.height = image.height;
+         var ctx = canvas.getContext('2d');
+        ctx.drawImage(image,100,100);
+      }
+    }
+    reader.readAsDataURL(file);
+
+  }
+})
+
+var canvas = document.getElementById('canvas');
+var dataURL = canvas.toDataURL();
+document.getElementById('url').innerHTML = dataURL;
